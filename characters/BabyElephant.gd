f12a6c74ef0a938c875c9e108ab90a4aae891440
Kevin Liang <kevinpliang@gmail.com>
2025-10-18 16:29:20 -0400

    Make y-sort work better with NPCs

--- characters/BabyElephant.gd
+++ characters/BabyElephant.gd
@@ -4,8 +4,8 @@ extends CharacterBody2D
 @export var min_move_distance: float = 600.0 # Much longer distances
 @export var max_move_distance: float = 1200.0 # Very long distances
 @export var arrival_threshold: float = 100.0 # Larger threshold
-@export var bounce_impulse: float = 1500.0
-@export var bounce_decay_rate: float = 1.0
+@export var bounce_impulse: float = 750.0
+@export var bounce_decay_rate: float = 2.0
 @export var avoidance_angle_degrees: float = 45.0 # Don't pick directions within this angle toward a lion
 @export var danger_distance: float = 600.0 # Consider lions within this distance when avoiding
 @export var avoidance_attempts: int = 12 # How many random samples to try for a safe direction
